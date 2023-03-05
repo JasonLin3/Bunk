9a5d2366121a371b156bc80348cfddfe6ba8186f
Chase Mathis <chasemathis@Chases-MacBook-Air.local>
2023-03-05 01:37:58 -0600

    Working on sign in screen

--- roommate-finder/src/components/Login.js
+++ roommate-finder/src/components/Login.js
@@ -1,11 +1,46 @@
-import React from 'react';
-import {Button} from 'react-bootstrap';
+import React from 'react'
 
-function Login(props) {
-    return <div>
-        <h1>Login</h1>
-        <Button onClick={props.change}>Login</Button>
-    </div>
-}
+export default function Login() {
 
-export default Login;
+  return (
+      <form className="col-md-4 m-5">
+        <h3 className='text-center'>Sign In</h3>
+        <div className="mb-3">
+          <label>Email address</label>
+          <input
+            type="email"
+            className="form-control"
+            placeholder="Enter email"
+          />
+        </div>
+        <div className="mb-3">
+          <label>Password</label>
+          <input
+            type="password"
+            className="form-control"
+            placeholder="Enter password"
+          />
+        </div>
+        <div className="mb-3">
+          <div className="custom-control custom-checkbox">
+            <input
+              type="checkbox"
+              className="custom-control-input"
+              id="customCheck1"
+            />
+            <label className="custom-control-label" htmlFor="customCheck1">
+              Remember me
+            </label>
+          </div>
+        </div>
+        <div className="d-grid">
+          <button type="submit" className="btn btn-primary">
+            Submit
+          </button>
+        </div>
+        <p className="forgot-password text-right">
+          Forgot <a href="#">password?</a>
+        </p>
+      </form>
+    )
+  }
