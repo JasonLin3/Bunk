df735d8eb0e14279f1a721362f8aa9cc26faadf2
Chase Mathis <chasemathis@Chases-MacBook-Air.local>
2023-03-05 02:16:21 -0600

    styling

--- roommate-finder/src/components/Login.js
+++ roommate-finder/src/components/Login.js
@@ -8,47 +8,34 @@ export default function Login(props) {
     const nav = useNavigate();
 
   return (
-      <form className="col-md-4 m-5">
-        <h3 className='text-center'>Sign In</h3>
-        <div className="mb-3">
-          <label>Email address</label>
-          <input
-            type="email"
-            className="form-control"
-            placeholder="Enter email"
-          />
-        </div>
-        <div className="mb-3">
-          <label>Password</label>
-          <input
-            type="password"
-            className="form-control"
-            placeholder="Enter password"
-          />
-        </div>
-        <div className="mb-3">
-          <div className="custom-control custom-checkbox">
+    <div className="row">
+        <form className="col-md-4 offset-md-4 my-5">
+            <h3 className='text-center'>Sign In</h3>
+            <div className="mb-3">
+            <label>Email address</label>
             <input
-              type="checkbox"
-              className="custom-control-input"
-              id="customCheck1"
+                type="email"
+                className="form-control"
+                placeholder="Enter email"
             />
-            <label className="custom-control-label" htmlFor="customCheck1">
-              Remember me
-            </label>
-          </div>
-        </div>
-        <div className="d-grid">
-          <button onClick={(e) => {
-            updateStatus();
-            nav('/');
-          }} type="submit" className="btn btn-primary">
-            Submit
-          </button>
-        </div>
-        <p className="forgot-password text-right">
-          Forgot <a href="#">password?</a>
-        </p>
-      </form>
+            </div>
+            <div className="mb-3">
+            <label>Password</label>
+            <input
+                type="password"
+                className="form-control"
+                placeholder="Enter password"
+            />
+            </div>
+            <div className="d-grid">
+            <button onClick={(e) => {
+                updateStatus();
+                nav('/');
+            }} type="submit" className="btn btn-primary">
+                Submit
+            </button>
+            </div>
+        </form>
+      </div>
     )
   }
